264d8d8b7d0de9bf806558e4551c9e42e94162d1
Amadinho94 <amadinho94@icloud.com>
2024-06-07 22:16:18 +0000

    Config fichier vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
   server: {
-      host: true,
-      port: 3000,
       proxy: {
         "/api": {
           target: "http://amadoudrame.ide.3wa.io:9000",
